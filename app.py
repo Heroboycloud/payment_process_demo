170c0be55e1143e0d36e354b6353421a468b5b18
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-06-22 09:08:18 +0100

    yeah .. ready

--- app.py
+++ app.py
@@ -8,7 +8,14 @@ app = Flask(__name__)
 load_dotenv()
 stripe.api_key= os.getenv('api_key')
 
-@app.route('/create-checkout-session', methods=['POST'])
+@app.route('/', methods=['POST','GET'])
+def get_detail():
+  return "Welcome to Stripe demo by Akindel...Visit /checkout to see it live"
+
+
+
+
+@app.route('/checkout', methods=['POST'])
 def create_checkout_session():
   session = stripe.checkout.Session.create(
     line_items=[{
@@ -22,8 +29,8 @@ def create_checkout_session():
       'quantity': 1,
     }],
     mode='payment',
-    success_url='https://localhost:8080/success.html',
-    cancel_url='https://localhost:8080/cancel.html',
+    success_url='https://hotel-master.vercel.app/success.html',
+    cancel_url='https://hotel-master.vercel.app/cancel.html',
   )
 
   return redirect(session.url, code=303)
